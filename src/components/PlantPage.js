4d36136a7252dfbdf98757308b71457d2e2f7567
mckinseyleckenby <leckenbym11@gmail.com>
2022-03-25 09:38:01 -0400

    commit tree cefd2c900e958db882b7a3f605e21c8f600d7c30

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]) 
+  //plants = what the data is 
+  //setPlants = what changed value is
+  //useState to empty version of that 
+  const url = "http://localhost:6001/plants"
+ 
+
+  
+  useEffect(() => {
+    fetch(url)
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+  }, []) 
+  //what going to have 
+
   return (
     <main>
+  
       <NewPlantForm />
       <Search />
       <PlantList />

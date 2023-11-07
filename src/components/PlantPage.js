ef9605afd9ed12908899980870cc3861fb3c7af4
Frank <francis.kipchumba@moringaschool.com>
2023-11-07 11:03:36 +0300

    implement fetch

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  let host = 'http://localhost:6001'
+
+  useEffect(()=>{
+    fetch(`${host}/plants`)
+    .then(res => res.json())
+    .then(data => console.log(data))
+  })
   return (
     <main>
       <NewPlantForm />

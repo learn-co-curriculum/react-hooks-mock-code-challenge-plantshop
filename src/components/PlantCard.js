d9764513e042907d8b75c333ae0ed0da02d906ac
Laura Berge <lauraeberge@gmail.com>
2024-01-19 15:17:53 -0600

    add data-testid to plantcard for test suite

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function PlantCard() {
   return (
-    <li className="card">
+    <li className="card" data-testid="plant-item">
       <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
       <h4>{"plant name"}</h4>
       <p>Price: {"plant price"}</p>

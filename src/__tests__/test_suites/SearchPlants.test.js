41d36fa3b8ec03856d54c6dd466df5728efe8d97
Laura Berge <lauraeberge@gmail.com>
2024-01-19 20:31:42 -0600

    fix indentation

--- src/__tests__/test_suites/SearchPlants.test.js
+++ src/__tests__/test_suites/SearchPlants.test.js
@@ -4,22 +4,22 @@ import App from '../../components/App';
 import '@testing-library/jest-dom';
 
 describe('4th Deliverable', () => {
-    test('filters plants by name on search', async () => {
-        global.setFetchResponse(global.basePlants)
-        const { getByPlaceholderText, queryAllByTestId } = render(<App />);
-        const searchInput = getByPlaceholderText('Type a name to search...');
-        fireEvent.change(searchInput, { target: { value: 'aloe' } });
+  test('filters plants by name on search', async () => {
+    global.setFetchResponse(global.basePlants)
+    const { getByPlaceholderText, queryAllByTestId } = render(<App />);
+    const searchInput = getByPlaceholderText('Type a name to search...');
+    fireEvent.change(searchInput, { target: { value: 'aloe' } });
+
+    await waitFor(() => {
+      const filteredPlants = queryAllByTestId('plant-item');
+      expect(filteredPlants).toHaveLength(1);
+    });
     
-        await waitFor(() => {
-          const filteredPlants = queryAllByTestId('plant-item');
-          expect(filteredPlants).toHaveLength(1);
-        });
-        
-        fireEvent.change(searchInput, { target: { value: 'p' } });
-        
-        await waitFor(() => {
-          const filteredPlants = queryAllByTestId('plant-item');
-          expect(filteredPlants).toHaveLength(3);
-        });
-      });
+    fireEvent.change(searchInput, { target: { value: 'p' } });
+    
+    await waitFor(() => {
+      const filteredPlants = queryAllByTestId('plant-item');
+      expect(filteredPlants).toHaveLength(3);
+    });
+  });
 })

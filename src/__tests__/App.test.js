effc2cb0d92687d67dd98b28edc8519078da2738
Laura Berge <lauraeberge@gmail.com>
2024-01-19 18:36:36 -0600

    add tests for rendering image, name, and price

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -61,6 +61,18 @@ describe('Plantsy App', () => {
     let { findAllByTestId } = render(<App />);
     const plantItems = await findAllByTestId('plant-item');
     expect(plantItems).toHaveLength(basePlants.length);
+
+    const plantNames = plantItems.map((item) => item.querySelector('h4').textContent);
+    const basePlantNames = basePlants.map((plant) => plant.name);
+    expect(plantNames).toEqual(basePlantNames);
+
+    const plantImages = plantItems.map((item) => item.querySelector('img').src.split('/')[-1]);
+    const basePlantImages = basePlants.map((plant) => plant.image.split('/')[-1]);
+    expect(plantImages).toEqual(basePlantImages);
+
+    const plantPrices = plantItems.map((item) => item.querySelector('p').textContent);
+    const basePlantPrices = basePlants.map((plant) => 'Price: ' + plant.price.toString());
+    expect(plantPrices).toEqual(basePlantPrices);
   });
 
   // Test 2: Add a new plant

5407d4534b38bc063996f54a7e9777bf28a1c3e1
Laura Berge <lauraeberge@gmail.com>
2024-01-19 20:32:38 -0600

    more indentation fixes

--- src/__tests__/test_suites/AllPlants.test.js
+++ src/__tests__/test_suites/AllPlants.test.js
@@ -4,22 +4,22 @@ import App from '../../components/App';
 import '@testing-library/jest-dom';
 
 describe('1st Deliverable', () => {
-    test('displays all plants on startup', async () => {
-        global.setFetchResponse(global.basePlants)
-        let { findAllByTestId } = render(<App />);
-        const plantItems = await findAllByTestId('plant-item');
-        expect(plantItems).toHaveLength(global.basePlants.length);
-    
-        const plantNames = plantItems.map((item) => item.querySelector('h4').textContent);
-        const basePlantNames = global.basePlants.map((plant) => plant.name);
-        expect(plantNames).toEqual(basePlantNames);
-    
-        const plantImages = plantItems.map((item) => item.querySelector('img').src.split('/')[-1]);
-        const basePlantImages = global.basePlants.map((plant) => plant.image.split('/')[-1]);
-        expect(plantImages).toEqual(basePlantImages);
-    
-        const plantPrices = plantItems.map((item) => item.querySelector('p').textContent);
-        const basePlantPrices = global.basePlants.map((plant) => 'Price: ' + plant.price.toString());
-        expect(plantPrices).toEqual(basePlantPrices);
-      });
+  test('displays all plants on startup', async () => {
+    global.setFetchResponse(global.basePlants)
+    let { findAllByTestId } = render(<App />);
+    const plantItems = await findAllByTestId('plant-item');
+    expect(plantItems).toHaveLength(global.basePlants.length);
+
+    const plantNames = plantItems.map((item) => item.querySelector('h4').textContent);
+    const basePlantNames = global.basePlants.map((plant) => plant.name);
+    expect(plantNames).toEqual(basePlantNames);
+
+    const plantImages = plantItems.map((item) => item.querySelector('img').src.split('/')[-1]);
+    const basePlantImages = global.basePlants.map((plant) => plant.image.split('/')[-1]);
+    expect(plantImages).toEqual(basePlantImages);
+
+    const plantPrices = plantItems.map((item) => item.querySelector('p').textContent);
+    const basePlantPrices = global.basePlants.map((plant) => 'Price: ' + plant.price.toString());
+    expect(plantPrices).toEqual(basePlantPrices);
+  });
 })
